3e4c7d52c1fe77db7826e8954a6ea0f312d5db39
James Coates <James@Lydias-MacBook-Pro.local>
2019-05-22 19:24:58 +0100

    fix api routes and add movie by title GET route

--- index.js
+++ index.js
@@ -1,65 +1,66 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // Movie List
 const movies = [
   {
-    Title: 'RAIDERS OF THE LOST ARK',
-    Year: '1982',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BMjA0ODEzMTc1Nl5BMl5BanBnXkFtZTcwODM2MjAxNA@@._V1_SX300.jpg',
+    title: 'RAIDERS OF THE LOST ARK',
+    year: '1982',
+    poster: 'https://m.media-amazon.com/images/M/MV5BMjA0ODEzMTc1Nl5BMl5BanBnXkFtZTcwODM2MjAxNA@@._V1_SX300.jpg',
   },
   {
-    Title: 'THE GIRL WITH THE DRAGON TATTOO',
-    Year: '2011',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BMTczNDk4NTQ0OV5BMl5BanBnXkFtZTcwNDAxMDgxNw@@._V1_SX300.jpg',
+    title: 'THE GIRL WITH THE DRAGON TATTOO',
+    year: '2011',
+    poster: 'https://m.media-amazon.com/images/M/MV5BMTczNDk4NTQ0OV5BMl5BanBnXkFtZTcwNDAxMDgxNw@@._V1_SX300.jpg',
   },
   {
-    Title: 'Lord of War',
-    Year: '2005',
-    Poster:
+    title: 'Lord of War',
+    year: '2005',
+    poster:
       'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
   },
   {
-    Title: "Breakfast At Tiffany's",
-    Year: '1961',
-    Poster:
+    title: "Breakfast At Tiffany's",
+    year: '1961',
+    poster:
       'https://m.media-amazon.com/images/M/MV5BNGEwMTRmZTQtMDY4Ni00MTliLTk5ZmMtOWMxYWMyMTllMDg0L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg',
   },
   {
-    Title: 'Moon',
-    Year: '2009',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BMTgzODgyNTQwOV5BMl5BanBnXkFtZTcwNzc0NTc0Mg@@._V1_SX300.jpg',
+    title: 'Moon',
+    year: '2009',
+    poster: 'https://m.media-amazon.com/images/M/MV5BMTgzODgyNTQwOV5BMl5BanBnXkFtZTcwNzc0NTc0Mg@@._V1_SX300.jpg',
   },
   {
-    Title: 'The Rocketeer',
-    Year: '1991',
-    Poster:
+    title: 'The Rocketeer',
+    year: '1991',
+    poster:
       'https://m.media-amazon.com/images/M/MV5BN2ZiMjkwNWYtZWRjNy00YTYxLWI1ZWYtODI0NTA5YTg4ZDIxXkEyXkFqcGdeQXVyNDA5ODIzMDk@._V1_SX300.jpg',
   },
   {
-    Title: 'Halloween',
-    Year: '1978',
-    Poster:
+    title: 'Halloween',
+    year: '1978',
+    poster:
       'https://m.media-amazon.com/images/M/MV5BNzk1OGU2NmMtNTdhZC00NjdlLWE5YTMtZTQ0MGExZTQzOGQyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
   },
   {
-    Title: 'The Truman Show',
-    Year: '1998',
-    Poster:
+    title: 'The Truman Show',
+    year: '1998',
+    poster:
       'https://m.media-amazon.com/images/M/MV5BMDIzODcyY2EtMmY2MC00ZWVlLTgwMzAtMjQwOWUyNmJjNTYyXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
   },
   {
-    Title: 'Goodfellas',
-    Year: '1990',
-    Poster:
+    title: 'Goodfellas',
+    year: '1990',
+    poster:
       'https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
   },
   {
-    Title: 'Pulp Fiction',
-    Year: '1994',
-    Poster:
+    title: 'Pulp Fiction',
+    year: '1994',
+    poster:
       'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
   },
 ];
@@ -69,6 +70,9 @@ app.use(express.static(`${__dirname}/public`));
 
 // Use Morgan Middlware for logging requests
 app.use(morgan('common'));
+
+// Use body-parser middleware
+app.use(bodyParser.json());
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
@@ -97,7 +101,8 @@ app.get('/documentation', (req, res) => {
 // Temporary API Endpoint Routes
 
 app.get('/movies/:title', (req, res) => {
-  res.send('Successful GET request on movie route');
+  const movieTitle = req.params.title;
+  res.json(movies.find(movie => movie.title === movieTitle));
 });
 
 app.get('/genres/:name', (req, res) => {
