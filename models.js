560be43a84de78d67473878d29205f95d15bd962
James Coates <James@Lydias-MacBook-Pro.local>
2019-05-26 18:44:15 +0100

    add DB models and schema

---
+++ models.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String,
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+    BirthYear: Date,
+    DeathYear: Date,
+  },
+  Actors: [String],
+  ImpagePath: String,
+  Featured: Boolean,
+});
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
