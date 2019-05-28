868430e0edf4f62f4300ed386d367cab94583be6
James Coates <James@Lydias-MacBook-Pro.local>
2019-05-28 18:18:59 +0100

    add passport strategies

---
+++ passport.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-console */
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const passportLocal = require('passport-local');
+const Models = require('./models.js');
+
+// User model
+const Users = Models.User;
+
+// Define passport strategies
+const LocalStrategy = passportLocal.Strategy;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'Username',
+      passwordField: 'Password',
+    },
+    (username, password, callback) => {
+      console.log(`${username}  ${password}`);
+      Users.findOne({ Username: username })
+        .then(user => {
+          if (!user) {
+            console.log('Incorrect username');
+            return callback(null, false, { message: 'Incorrect username or password.' });
+          }
+          console.log('Finished!');
+          return callback(null, user);
+        })
+        .catch(err => callback(err));
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'poppydog',
+    },
+    (jwtPayload, callback) =>
+      Users.findById(jwtPayload._id)
+        .then(user => callback(null, user))
+        .catch(err => callback(err))
+  )
+);
