ae01aa91be359c408e4adf363cb621a8074edf2e
James Coates <James@Lydias-MacBook-Pro.local>
2019-05-29 18:34:35 +0100

    refactor keys lower case

--- models.js
+++ models.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-  Title: { type: String, required: true },
-  Description: { type: String, required: true },
-  Genre: {
-    Name: String,
-    Description: String,
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  genre: {
+    name: String,
+    description: String,
   },
-  Director: {
-    Name: String,
-    Bio: String,
-    BirthYear: Date,
-    DeathYear: Date,
+  director: {
+    name: String,
+    bio: String,
+    birthYear: Date,
+    deathYear: Date,
   },
-  Actors: [String],
-  ImpagePath: String,
-  Featured: Boolean,
+  actors: [String],
+  impagePath: String,
+  featured: Boolean,
 });
 
 const userSchema = new mongoose.Schema({
-  Username: { type: String, required: true },
-  Password: { type: String, required: true },
-  Email: { type: String, required: true },
-  Birthday: Date,
-  FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  birthday: Date,
+  favouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
 
 const Movie = mongoose.model('Movie', movieSchema);
