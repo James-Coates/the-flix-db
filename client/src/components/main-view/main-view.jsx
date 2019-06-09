24496edfcedfbe85a6de7105a31a7eb56923c2a5
James Coates <James@Lydias-MacBook-Pro.local>
2019-06-09 13:38:17 +0100

    add basic React structure

---
+++ client/src/components/main-view/main-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+
+class MainView extends React.Component {
+  constructor() {
+    // Call and initialise superclass constructor
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div className="main-view" />;
+  }
+}
