cac631e8d6fb7fd76ed212df3f1c7a5892493444
James Coates <James@Lydias-MacBook-Pro.local>
2019-06-21 18:32:14 +0100

    migrate to create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { MainView } from './components/main-view/main-view';
+
+class App extends Component {
+  render() {
+    return(
+    <MainView />
+    )
+  }
+}
+
+export default App;
