a3dbe8aeeef659c955e321332f614f801713411e
James Coates <James@Lydias-MacBook-Pro.local>
2019-06-23 18:24:29 +0100

    add addToFavourites and deleteFavouriteMovie functions and styling

--- client/src/components/profile-view/profile-view.jsx
+++ client/src/components/profile-view/profile-view.jsx
@@ -1,47 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import PropTypes from 'prop-types';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form } from 'react-bootstrap';
+import Moment from 'react-moment';
+import moment from 'moment';
+import { MovieCard } from '../movie-card/movie-card';
+import './profile-view.scss';
 
 export function ProfileView( props ) {
 
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
-  const [edit, setEdit] = useState('');
+  // const [edit, setEdit] = useState('');
+  const [favouriteMovies, setFavouriteMovies] = useState('');
 
+  const {movies, addToFavourites, deleteFavouriteMovie} = props;
   const apiUrl = 'https://theflixdb.herokuapp.com'
 
     // Load users on mount
     useEffect(() => {
       axios.get(`${apiUrl}/users`)
       .then(response => {
-        const user = response.data.find(u => u.username === localStorage.user);
+        const user = response.data.find(u => u.username === props.user);
         setUsername(user.username);
         setEmail(user.email);
-        setBirthday(user.birthday);
+        setBirthday(moment(user.birthday, moment.ISO_8601).format('YYYY-MM-DD'));
+        setFavouriteMovies(user.favouriteMovies);
       })
       .catch(err => console.log('Can\'t get users'));
-    }, [])
+    }, [props.user])
 
   return(
     <div className="profile-view">
       <Container>
         <Form>
           <Row>
+            <Col xs={12}>
+              <div className="username__container">
+                <h1>{username}</h1>
+                <Form.Control 
+                    type="username"
+                    placeholder="Username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                    className="form-input"
+                />
+              </div>
+            </Col>
             <Col md={6}>
-            <div className="username">
-              {username}
-              <Form.Control 
-                  type="username"
-                  placeholder="Username"
-                  value={username}
-                  onChange={e => setUsername(e.target.value)}
-                  className="form-input"
-              />
-            </div>
             <div className="email">
-              {email}
+              Email: 
+              <h2>{email}</h2>
               <Form.Control 
                   type="email"
                   placeholder="Email"
@@ -50,8 +59,13 @@ export function ProfileView( props ) {
                   className="form-input"
               />
             </div>
+            </Col>
+            <Col md={6}>
             <div className="birthday">
-              {birthday}
+              Birthday:
+              <h2>
+                <Moment format="DD/MM/YYYY">{moment(birthday)}</Moment>
+              </h2>
               <Form.Control 
                   type="date"
                   placeholder="Birthday"
@@ -61,14 +75,21 @@ export function ProfileView( props ) {
               />
             </div>
             </Col>
-            <Col md={6}>Favourite Movies</Col>
+              <div className="favourite-movies">
+                <h2>Favourite Movies:</h2>
+                <Row>
+                  {Array.from(favouriteMovies).map((movieId) => <MovieCard 
+                    key={movieId} 
+                    movie={movies.find(m => m._id === movieId)} 
+                    addToFavourites={(movieId) => addToFavourites(movieId)}
+                    deleteFavouriteMovie={(movieId) => deleteFavouriteMovie(movieId)}
+                    profileView = {true} 
+                    />)}
+                </Row>
+              </div>
           </Row>
         </Form>
       </Container>
     </div>
   )
 } 
-
-ProfileView.propTypes = {
-  user: PropTypes.string.isRequired
-}
