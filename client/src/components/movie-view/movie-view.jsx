32e17400822b23a6a48b3d02bfbb0e541a0671ac
James Coates <James@Lydias-MacBook-Pro.local>
2019-06-25 19:17:46 +0100

    alter movie-view for redux

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -4,71 +4,71 @@ import PropTypes from 'prop-types';
 import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './movie-view.scss';
+import { connect } from 'react-redux';
 
-export class MovieView extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+function MovieView(props) {
+  const { movies, movieId } = props;
 
-  render() {
-    const { movie } = this.props;
+  if (!movies || !movies.length) return null;
 
-    if (!movie) return null;
-    return (
-      <div className="movie-view">
-        <Jumbotron>
-          <Container>
-            <h1 className="movie-view__heading">{movie.title}</h1>
-            <Row>
-              <Col md={4}>
-                <img src={movie.imagePath} alt="Movie Poster" className="movie-poster" />
-              </Col>
-              <Col md={8}>
-                <div className="movie-view__trailer">
-                  {/* Temporary static iframe */}
-                  <iframe
-                    title="movie-trailer"
-                    id="ytplayer"
-                    type="text/html"
-                    width="100%"
-                    height="100%"
-                    src="https://www.youtube.com/embed/GLPJSmUHZvU?autoplay=0"
-                    frameBorder="0"
-                  />
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </Jumbotron>
-        <Container className="movie-view__info">
+  const movie = movies.find(m => m._id == movieId);
+
+  return(
+    <div className="movie-view">
+      <Jumbotron>
+        <Container>
+          <h1 className="movie-view__heading">{movie.title}</h1>
           <Row>
-            <Col md={8}>
-              <h3>Description:</h3>
-              <div className="value">{movie.description}</div>
-            </Col>
             <Col md={4}>
-              <div className="movie-desc__holder">
-                <div className="movie-desc">
-                  <div className="movie-desc__label">Genre:</div>
-                  <Link to={`/genres/${movie.genre.name}`}>
-                    <div className="movie-desc__value">{movie.genre.name}</div>
-                  </Link>
-                </div>
-                <div className="movie-desc">
-                  <div className="movie-desc__label">Director:</div>
-                  <Link to={`/directors/${movie.director.name}`}>
-                    <div className="movie-desc__value">{movie.director.name}</div>
-                  </Link>
-                </div>
+              <img src={movie.imagePath} alt="Movie Poster" className="movie-poster" />
+            </Col>
+            <Col md={8}>
+              <div className="movie-view__trailer">
+                {/* Temporary static iframe */}
+                <iframe
+                  title="movie-trailer"
+                  id="ytplayer"
+                  type="text/html"
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/GLPJSmUHZvU?autoplay=0"
+                  frameBorder="0"
+                />
               </div>
             </Col>
           </Row>
         </Container>
-      </div>
-    );
-  }
+      </Jumbotron>
+      <Container className="movie-view__info">
+        <Row>
+          <Col md={8}>
+            <h3>Description:</h3>
+            <div className="value">{movie.description}</div>
+          </Col>
+          <Col md={4}>
+            <div className="movie-desc__holder">
+              <div className="movie-desc">
+                <div className="movie-desc__label">Genre:</div>
+                <Link to={`/genres/${movie.genre.name}`}>
+                  <div className="movie-desc__value">{movie.genre.name}</div>
+                </Link>
+              </div>
+              <div className="movie-desc">
+                <div className="movie-desc__label">Director:</div>
+                <Link to={`/directors/${movie.director.name}`}>
+                  <div className="movie-desc__value">{movie.director.name}</div>
+                </Link>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+
 }
+
+export default connect(({movies}) => ({movies}))(MovieView)
 
 MovieView.propTypes = {
   movie: PropTypes.shape({
