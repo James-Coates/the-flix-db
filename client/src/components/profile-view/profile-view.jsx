692a26745eb6bc3ff0de07569414d35535562326
James Coates <James@Lydias-MacBook-Pro.local>
2019-06-27 20:43:57 +0100

    fix edit user password bug

--- client/src/components/profile-view/profile-view.jsx
+++ client/src/components/profile-view/profile-view.jsx
@@ -1,56 +1,78 @@
 import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import Moment from 'react-moment';
 import moment from 'moment';
 import { connect } from 'react-redux';
-import { MovieCard } from '../movie-card/movie-card';
+import MovieCard from '../movie-card/movie-card';
 import './profile-view.scss';
 
-function ProfileView( props ) {
+import { setUser, setMovies } from '../../actions/actions';
 
-  const { movies, user } = props;
-  // const  mapStateToProps = state => {
-  //   const { movies, user } = state;
-  // }
+function ProfileView( {movies, user, setUser, setMovies} ) {
 
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
-  // const [edit, setEdit] = useState('');
+  const [password, setPassword] = useState('');
   const [favouriteMovies, setFavouriteMovies] = useState('');
 
-  // const {movies, addToFavourites, deleteFavouriteMovie} = props;
   const apiUrl = 'https://theflixdb.herokuapp.com'
 
-    // Load users on mount
-    useEffect(() => {
-      axios.get(`${apiUrl}/users`)
-      .then(response => {
-        const user = response.data.find(u => u.username === props.user);
-        setUsername(user.username);
-        setEmail(user.email);
-        setBirthday(moment(user.birthday, moment.ISO_8601).format('YYYY-MM-DD'));
-        setFavouriteMovies(user.favouriteMovies);
-      })
-      .catch(err => console.log('Can\'t get users'));
-    }, [props.user])
+  // Load users on mount
+  useEffect(() => {
+    axios.get(`${apiUrl}/users`)
+    .then(response => {
+      const userObject = response.data.find(u => u.username === user);
+      setUsername(userObject.username);
+      setEmail(userObject.email);
+      setBirthday(moment(userObject.birthday, moment.ISO_8601).format('YYYY-MM-DD'));
+      setPassword(userObject.password);
+      setFavouriteMovies(userObject.favouriteMovies);
+    })
+    .catch(err => {
+      if(!user) return null;
+      console.log('Can\'t get users')
+    });
+  }, [user])
 
-    const handleSubmit = e => {
-      e.preventDefault();
-      axios.put(`${apiUrl}/users/${localStorage.user}`, {
-        username,
-        email,
-        birthday
-      }, {
-        headers: { Authorization: `Bearer ${localStorage.token}` }
-      })
-      .then(response => {
-        localStorage.setItem('user', username);
-        document.location.reload();
-      })
-      .catch(e => console.log('Failed to update user'));
-    }
+  // Handle edit user properties
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.put(`${apiUrl}/users/${localStorage.user}`, {
+      username,
+      email,
+      birthday,
+      password
+    }, {
+      headers: { Authorization: `Bearer ${localStorage.token}` }
+    })
+    .then(response => {
+      localStorage.setItem('user', username);
+      document.location.reload();
+    })
+    .catch(e => console.log('Failed to update user'));
+  }
+
+  // Delete user
+  const handleDelete = e => {
+    axios.delete(`${apiUrl}/users/${username}`, {
+      headers: { Authorization: `Bearer ${localStorage.token}` }
+    })
+    .then(response => {
+      console.log('User deleted');
+      localStorage.clear();
+      setUser('');
+      setMovies([]);
+    })
+    .catch(e => console.log('Failed to delete user'));
+  }
+
+  if(!user) return <Redirect to="/" />
+  if(!movies.length) return null;
 
   return(
     <div className="profile-view">
@@ -99,6 +121,7 @@ function ProfileView( props ) {
             </Col>
             <Col xs={12}>
               <Button onClick={handleSubmit}>Submit</Button>
+              <Button variant="danger" onClick={handleDelete}>Delete</Button>
             </Col>
           </Row>
         </Form>
@@ -108,7 +131,6 @@ function ProfileView( props ) {
             {Array.from(favouriteMovies).map((movieId) => <MovieCard 
               key={movieId} 
               movie={movies.find(m => m._id === movieId)} 
-
               profileView = {true} 
               />)}
           </Row>
@@ -118,4 +140,9 @@ function ProfileView( props ) {
   )
 } 
 
-export default connect(({movies, user}) => ({movies, user}))(ProfileView);
+ProfileView.propTypes = {
+  movies: PropTypes.array.isRequired,  
+  user: PropTypes.string.isRequired,
+}
+
+export default connect(({movies, user}) => ({movies, user}), {setUser, setMovies})(ProfileView);
